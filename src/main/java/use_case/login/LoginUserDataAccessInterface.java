05c3dd4d7ed72d8bce2356f5ff1076caddccca26
wilonmiller <150298993+wiltonmiller@users.noreply.github.com>
2024-10-28 15:53:15 -0400

    Added missing files for Task 2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user with the given username.
+     * @param name the username to set
+     */
+    void setCurrentUser(String name);
 }
